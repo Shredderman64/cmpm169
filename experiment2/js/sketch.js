dad4bc50ff09cb24dc48c37322785251476cf551
Shredderman64 <31966305+Shredderman64@users.noreply.github.com>
2025-01-28 14:18:02 -0800

    Experiement 3

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -15,6 +15,10 @@ let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
 
+let x, y;
+let pods = [];
+let clusters = [];
+
 class MyClass {
     constructor(param1, param2) {
         this.property1 = param1;
@@ -49,31 +53,103 @@ function setup() {
     resizeScreen();
   });
   resizeScreen();
+
+  ellipseMode(RADIUS);
+  x = centerHorz;
+  y = centerVert;
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(220);    
+  clear();
+  background(200);
   // call a method on the instance
-  myInstance.myMethod();
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
+  for (let cluster of clusters) {
+    cluster.grow();
+    fill(0, random(100, 255), random(100, 255));
+    cluster.draw();
+  }
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
+  fill("white");
+  for (let pod of pods) {
+    pod.draw();
+    let distance = dist(x, y, pod.x(), pod.y());
+    if (distance <= pod.radius()) {
+      let index = pods.indexOf(pod);
+      pods.splice(index, 1);
+
+      let cluster = createSpores(pod.x(), pod.y());
+      clusters.push(cluster);
+    }
+  }
+
+  stroke("black");
+  ellipse(x, y, 10);
+
+  x += random(-5, 5);
+  y += random(-5, 5);
+  x = constrain(x, 0, width);
+  y = constrain(y, 0, height);
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+  let pod = createPod(mouseX, mouseY, 20);
+  pod.draw();
+  pods.push(pod);
+}
+
+function createPod(x, y, r) {
+  return {
+    draw() { ellipse(x, y, r); },
+    x() { return x; },
+    y() { return y; },
+    radius() { return r; },
+  }
+}
+
+function createSpores(xInit, yInit) {
+  let maxSpores = 200;
+  let currentSpores = 0;
+  let x = [xInit];
+  let y = [yInit];
+  let r = [20];
+
+  return {
+    draw() {
+      for (let i = 0; i < currentSpores; i++) {
+        ellipse(x[i], y[i], r[i]);
+      }
+    },
+    grow() {
+      if (currentSpores >= maxSpores) return;
+
+      let newR = random(5, 15);
+      let newX = random(newR, width - newR);
+      let newY = random(newR, height - newR);
+
+      // find closest spore to new position
+      let closestDist = Number.MAX_VALUE;
+      let closestIndex = 0;
+      for (let i = 0; i < currentSpores; i++) {
+        let newDist = dist(newX, newY, x[i], y[i]);
+        if (newDist < closestDist) {
+          closestDist = newDist;
+          closestIndex = i;
+        }
+      }
+
+      // find the angle between the two spores
+      let angle = atan2(newY - y[closestIndex], newX - x[closestIndex]);
+
+      let hyp = r[closestIndex] + newR;
+      x[currentSpores] = x[closestIndex] + cos(angle) * hyp;
+      y[currentSpores] = y[closestIndex] + sin(angle) * hyp;
+      r[currentSpores] = newR;
+
+      currentSpores++;
+    },
+  }
 }
